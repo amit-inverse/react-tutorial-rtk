cdb25440ed03468830344633af241032cb8bcfca
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-25 16:28:51 +0530

    Understanding Slices

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,15 @@ const songsSlice = createSlice({
     name: 'song',
     initialState: [],
     reducers: {
+        // 'song' + '/' + 'addSong' = 'song/addSong'
         addSong(state, action) {
+            // STATE IS NOT THE BIG STATE OBJECT
+            // IN THE STORE
+            // IT IS THE PIECE OF STATE MANAGED
+            // BY THIS REDUCER
             state.push(action.payload);
         },
+        // 'song' + '/' + 'removeSong' = 'song/removeSong'
         removeSong(state, action) {
             //
         },
