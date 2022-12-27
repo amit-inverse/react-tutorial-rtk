24a13a2adc9540a4b28764a34c4e9c4509ca209b
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-27 21:42:54 +0530

    Connecting React to Redux

--- src/store/index.js
+++ src/store/index.js
@@ -25,19 +25,4 @@ const store = configureStore({
     },
 });
 
-// console.log(songsSlice.actions.addSong('Some Song!'));
-
-const startingState = store.getState();
-console.log(JSON.stringify(startingState));
-
-// store.dispatch({
-//     type: 'song/addSong',
-//     payload: 'New Song!!!',
-// });
-
-store.dispatch(
-    songsSlice.actions.addSong('Some Song!')
-)
-
-const finalState = store.getState();
-console.log(JSON.stringify(finalState));
+export { store };
